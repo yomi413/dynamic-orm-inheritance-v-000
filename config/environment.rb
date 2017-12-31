e72bd90c3401c30d55f3415205b44d042e550943
Yomaira Escano <yjescano@gmail.com>
2017-12-31 03:33:20 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'sqlite3'
 
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
+
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
